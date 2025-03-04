502e6616a30e40989ae1f3d1bec1dd6ca288b62d
Borisa <borisa.cajic@gmail.com>
2025-03-04 22:57:13 +0100

    Initial commit

---
+++ src/test/java/borisa/com/example/springboot_kafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package borisa.com.example.springboot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
